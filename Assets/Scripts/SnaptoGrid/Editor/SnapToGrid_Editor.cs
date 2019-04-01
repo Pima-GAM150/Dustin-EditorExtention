a66d75192e0ae6a83e324643e4286e3ee15b7ad9
Unknown <dreed12@mail.pima.edu>
2019-04-01 16:45:35 -0700

    created Snap to grid scripts

---
+++ Assets/Scripts/SnaptoGrid/Editor/SnapToGrid_Editor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class SnapToGrid_Editor : MonoBehaviour
+{
+	#region Variables
+	
+		//public
+		
+		//private
+		
+	#endregion
+	
+	#region UnityFunctions
+	
+		void Start()
+		{
+			
+		}
+
+		void Update()
+		{
+			
+		}
+		
+	#endregion
+}
---
+++ Assets/Scripts/SnaptoGrid/Editor/SnapToGrid_MenuItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class SnapToGrid_MenuItem : MonoBehaviour
+{
+	#region Variables
+	
+		//public
+		
+		//private
+		
+	#endregion
+	
+	#region UnityFunctions
+	
+		void Start()
+		{
+			
+		}
+
+		void Update()
+		{
+			
+		}
+		
+	#endregion
+}
